8af9d50853737e0ceca43da3ce53a6538801a4ff
Piet Schijven <pietschijven@live.com>
2018-02-18 11:31:28 +0100

    Fixed number data type for pgsql

--- src/main/resources/db/migration/V1__Init.sql
+++ src/main/resources/db/migration/V1__Init.sql
@@ -1,6 +1,6 @@
 create table transactions (
   id varchar(255) not null PRIMARY KEY,
-  amount number not null,
+  amount decimal(20,2) not null,
   description VARCHAR(255),
   creation_date TIMESTAMP not null,
   username varchar(20) not null
