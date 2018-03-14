332ed7bc542c896dc5aadb8db552a8457ec4372a
Piet Schijven <pietschijven@live.com>
2018-03-14 10:57:08 +0100

    Fixed issue with Spring CrontabSequenceGenerator

--- src/main/java/de/pschijven/haushaltservice/reporting/ReportScheduler.java
+++ src/main/java/de/pschijven/haushaltservice/reporting/ReportScheduler.java
@@ -1,10 +1,16 @@
 package de.pschijven.haushaltservice.reporting;
 
 import de.pschijven.haushaltservice.configuration.ReportProperties;
+import de.pschijven.haushaltservice.util.DateProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+import static de.pschijven.haushaltservice.util.DateProvider.TIMEZONE;
 
 @Component
 public class ReportScheduler {
@@ -13,23 +19,42 @@ public class ReportScheduler {
 
     private final ReportEmail reportEmail;
     private final ReportProperties properties;
+    private final DateProvider dateProvider;
 
-    public ReportScheduler(ReportEmail reportEmail, ReportProperties properties) {
+    public ReportScheduler(ReportEmail reportEmail, ReportProperties properties, DateProvider dateProvider) {
         this.reportEmail = reportEmail;
         this.properties = properties;
+        this.dateProvider = dateProvider;
     }
 
-    @Scheduled(cron = "${report.schedule}", zone = "Europe/Berlin")
-    public void sendReport() {
+    @Scheduled(cron = "${report.schedule}", zone = TIMEZONE)
+    public void scheduleReport() {
+        if (isLastDayOfMonth()) {
+            sendReport();
+        }
+    }
+
+    private void sendReport() {
         String[] recipients = properties.getRecipients();
         if (recipients != null && recipients.length > 0) {
             for (String recipient : recipients) {
-                try {
-                    reportEmail.createAndSendTo(recipient);
-                } catch (Exception e) {
-                    LOGGER.error("Error while sending report: " + e.getMessage(), e);
-                }
+                sendReportTo(recipient);
             }
         }
+    }
+
+    private void sendReportTo(String recipient) {
+        try {
+            reportEmail.createAndSendTo(recipient);
+        } catch (Exception e) {
+            LOGGER.error("Error while sending report: " + e.getMessage(), e);
+        }
+    }
+
+    private boolean isLastDayOfMonth() {
+        LocalDate now = dateProvider.currentLocalDate();
+        int currentDay = now.getDayOfMonth();
+        int lastDayOfMonth = now.with(TemporalAdjusters.lastDayOfMonth()).getDayOfMonth();
+        return currentDay == lastDayOfMonth;
     }
 }
--- src/main/java/de/pschijven/haushaltservice/util/DateProvider.java
+++ src/main/java/de/pschijven/haushaltservice/util/DateProvider.java
@@ -3,12 +3,15 @@ package de.pschijven.haushaltservice.util;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
 
 @Component
 public class DateProvider {
 
+    public static final String TIMEZONE = "Europe/Berlin";
+
     public LocalDate currentLocalDate() {
-        return LocalDate.now();
+        return LocalDate.now(ZoneId.of("Europe/Berlin"));
     }
 
 }
