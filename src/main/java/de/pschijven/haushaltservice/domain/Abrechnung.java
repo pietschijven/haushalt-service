299d2036723bad9bbd3e7d75a5aa8c222d5ddb36
Piet Schijven <pietschijven@live.com>
2018-06-01 14:43:45 +0200

    Listing salary in Abrechnung

--- src/main/java/de/pschijven/haushaltservice/domain/Abrechnung.java
+++ src/main/java/de/pschijven/haushaltservice/domain/Abrechnung.java
@@ -1,6 +1,7 @@
 package de.pschijven.haushaltservice.domain;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
 
 import static java.util.stream.Collectors.reducing;
@@ -8,10 +9,18 @@ import static java.util.stream.Collectors.reducing;
 public class Abrechnung {
     private Map<String, BigDecimal> subtotals;
     private Map<String, BigDecimal> amountToPay;
+    private List<UserMetadata> metadata;
 
-    public Abrechnung(Map<String, BigDecimal> subtotals, Map<String, BigDecimal> amountToPay) {
+    public Abrechnung(Map<String, BigDecimal> subtotals,
+                      Map<String, BigDecimal> amountToPay,
+                      List<UserMetadata> metadata) {
         this.subtotals = subtotals;
         this.amountToPay = amountToPay;
+        this.metadata = metadata;
+    }
+
+    public List<UserMetadata> getMetadata() {
+        return metadata;
     }
 
     public Map<String, BigDecimal> getSubtotals() {
